9f99c1468b06935a4648e5c0bf0c89c50bfc1a7e
Sakshi-Void <singhsakshi0430@gmail.com>
2025-09-24 18:30:59 +0530

    initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,3 @@
-// import React from 'react';
-// import Header from './components/Header';
-// import HeroSection from './components/HeroSection';
-// import PopularCategories from './components/PopularCategories';
-// import WhyChooseUs from './components/WhyChooseUs';
-// import LifestyleNavigation from './components/LifestyleNavigation';
-// import BestSellers from './components/BestSellers';
-// import LifestyleHighlights from './components/LifestyleHighlights';
-// import Testimonials from './components/Testimonials';
-// import Footer from './components/Footer';
-// import FloatingAssistant from './components/FloatingAssistant';
-
-// function App() {
-//   return (
-//     <div className="min-h-screen bg-white">
-//       <Header />
-//       <main>
-//         <HeroSection />
-//         <PopularCategories />
-//         <WhyChooseUs />
-//         <LifestyleNavigation />
-//         <BestSellers />
-//         <LifestyleHighlights />
-//         <Testimonials />
-//       </main>
-//       <Footer />
-//       <FloatingAssistant />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
